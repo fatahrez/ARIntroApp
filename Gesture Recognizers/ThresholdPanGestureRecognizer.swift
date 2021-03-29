734f152c2f91e8882d1aa23da8ca160c83d75fb2
fatahrez <fertahrez@gmail.com>
2021-03-30 00:39:37 +0300

    finish on ViewController by adding SessionInfo

---
+++ Gesture Recognizers/ThresholdPanGestureRecognizer.swift
@@ -0,0 +1,8 @@
+//
+//  ThresholdPanGestureRecognizer.swift
+//  Matttress
+//
+//  Created by Abdulfatah Mohamed on 30/03/2021.
+//
+
+import Foundation
--- Matttress/AppDelegate.swift
+++ Matttress/AppDelegate.swift
@@ -66,7 +66,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        if let viewController = seld.window?.rootViewController as?
+        if let viewController = self.window?.rootViewController as?
             ViewController {
             viewController.blurView?.isHidden = true
         }
