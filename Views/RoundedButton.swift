2f4b892b21e21737846bfa235c4faa61309ad976
fatahrez <fertahrez@gmail.com>
2021-03-30 02:24:18 +0300

    Add view group for buttons in UI

---
+++ Views/RoundedButton.swift
@@ -0,0 +1,46 @@
+//
+//  RoundedButton.swift
+//  Matttress
+//
+//  Created by Abdulfatah Mohamed on 30/03/2021.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        backgroundColor = .appBlue
+        layer.cornerRadius = 8
+        clipsToBounds = true
+        setTitleColor(.white, for: [])
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+    }
+    
+    override var isEnabled: Bool {
+        didSet {
+            backgroundColor = isEnabled ? .appBlue : .appGray
+        }
+    }
+    
+    var toggledOn: Bool = true {
+        didSet {
+            if !isEnabled {
+                backgroundColor = .appGray
+                return
+            }
+            backgroundColor = toggledOn ? .appBlue : .appLightBlue
+        }
+    }
+}
