63b5ff60130caddeb26885e44fc0e3bc49c0e3b5
fatahrez <fertahrez@gmail.com>
2021-03-30 02:35:56 +0300

    add Ar camera tracking state extension

---
+++ Utility Extensions/ARCameraTrackingState.swift
@@ -0,0 +1,46 @@
+//
+//  ARCameraTrackingState.swift
+//  Matttress
+//
+//  Created by Abdulfatah Mohamed on 30/03/2021.
+//
+
+import Foundation
+import ARKit
+
+extension ARCamera.TrackingState {
+    var presentationString: String {
+        switch self {
+        case .notAvailable:
+            return "ARKit tracking UNAVAILABLE"
+        case .normal:
+            return "ARKit tracking NORMAL"
+        case .limited(let reason):
+            switch reason {
+            case .excessiveMotion:
+                return "ARKit tracking LIMITED: Excessive motion"
+            case .insufficientFeatures:
+                return "ARKit tracking LIMITED: Low detail"
+            case .initializing:
+                return "ARKit is initializing"
+            case .relocalizing:
+                return "ARKit is relocalizing"
+            }
+        }
+    }
+    
+    var recommendation: String? {
+        switch self {
+        case .limited(.excessiveMotion):
+            return "Try slowing down your movement, or reset the session."
+        case .limited(.insufficientFeatures):
+            return "Try pointing at a flat surface, or reset the session."
+        case .limited(.initializing):
+            return "Try moving left or right, reset the session."
+        case .limited(.relocalizing):
+            return "Try returning to the location where you left off."
+        default:
+            return nil
+        }
+    }
+}
